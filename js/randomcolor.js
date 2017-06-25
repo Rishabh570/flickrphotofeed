428c200fddeef34c2b62ed13d264d46fdd436b50
Rishabh Rawat <www.rishabhrawat570@gmail.com>
2017-06-25 16:15:50 +0530

    Update randomcolor.js

--- js/randomcolor.js
+++ js/randomcolor.js
@@ -1,3 +1,6 @@
-$('#input').keypress(function() {
-	console.log('done');
-});
+document.getElementById('input').onmouseover = function() {
+	var red = Math.floor(Math.random() * 256);
+	var green = Math.floor(Math.random() * 256);
+	var blue = Math.floor(Math.random() * 256);
+	document.getElementById('main-heading').style.color = "rgb(red,green,blue)";
+};
