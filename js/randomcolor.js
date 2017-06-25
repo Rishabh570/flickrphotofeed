3589f7e653bd8fb581dead442db188dce1d50543
Rishabh Rawat <www.rishabhrawat570@gmail.com>
2017-06-25 16:22:57 +0530

    Update randomcolor.js

--- js/randomcolor.js
+++ js/randomcolor.js
@@ -1,6 +1,15 @@
-document.getElementById('input').onmouseover = function() {
+
+	$('#input').mouseover(function() {
 	var red = Math.floor(Math.random() * 256);
 	var green = Math.floor(Math.random() * 256);
 	var blue = Math.floor(Math.random() * 256);
-	document.getElementById('main-heading').style.color = "rgb(red,green,blue)";
-};
+	$('#main-heading').css("opacity", 1);
+	});
+
+	$('#input').mouseleave(function() {
+	var red = Math.floor(Math.random() * 256);
+	var green = Math.floor(Math.random() * 256);
+	var blue = Math.floor(Math.random() * 256);
+	$('#main-heading').css("opacity", 0.6);
+	});
+	
