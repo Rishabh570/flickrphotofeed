c7dcfa3f5af69a1aa7c04609618338c69e6d7ec2
Rishabh Rawat <www.rishabhrawat570@gmail.com>
2018-03-14 13:55:17 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -33,15 +33,34 @@ $('form').submit(function (evt) {
 
 // Fire AJAX every time page reaches 70% of height ##########################################
     $(document).scroll(function(e){
+        var searchTerm = $('#input').val();
+	var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+	var animal = searchTerm;
+	var flickrOptions = {
+		tags: animal,
+		format: "json",
+	};
 
         if (processing) {
             return false;
 	}
         if ( $(document).scrollTop() >= ($(document).height() - $(window).height())) {
-	    console.log('reached');
-            processing = true;
-            $.getJSON(flickrAPI, flickrOptions, displayPhotos);
-	    processing = false;
+	        console.log('reached');
+                processing = true;
+
+		// AJAX Starts
+		function displayPhotos(data) {
+			var photoHTML += '<ul>';
+			$.each(data.items, function(i,photo) {
+				photoHTML += '<li class="grid">';
+				photoHTML += '<a href="' + photo.link + '" class="image">';
+				photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+			});
+			photoHTML += '</ul>';
+			$('#photos').html(photoHTML);
+		}
+		$.getJSON(flickrAPI, flickrOptions, displayPhotos);
+		processing = false;
         }
     });
 //############################################################################################
