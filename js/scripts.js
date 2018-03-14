c3a055bf1dd2505fa9a32bf4cf275d049bfc9371
Rishabh Rawat <www.rishabhrawat570@gmail.com>
2018-03-14 13:14:25 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -30,7 +30,29 @@ $('form').submit(function (evt) {
 
 // Fire AJAX every time page reaches 70% of height
 if ($(window).scrollTop() >= ($(document).height() - $(window).height())*0.7) {
-    $.getJSON(flickrAPI, flickrOptions, displayPhotos);
+    	var searchTerm = $('#input').val();
+
+	// AJAX Starts
+
+	var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+	var animal = searchTerm;
+	var flickrOptions = {
+		tags: animal,
+		format: "json",
+
+	};
+	function displayPhotos(data) {
+		var photoHTML = '<ul>';
+		$.each(data.items, function(i,photo) {
+			photoHTML += '<li class="grid">';
+			photoHTML += '<a href="' + photo.link + '" class="image">';
+			photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+		});
+		photoHTML += '</ul>';
+		$('#photos').html(photoHTML);
+	}
+	
+	$.getJSON(flickrAPI, flickrOptions, displayPhotos);
 }
 	
 	
